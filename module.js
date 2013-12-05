4aa5c74de78f1a41049549a1fdabd8a39ad01171
Sukharev Kirill <SukharevKirill@gmail.com>
2013-12-05 12:36:29 +0400

    Module remove exit on .muon pid data, move it to m-start command

--- module.js
+++ module.js
@@ -1,10 +1,4 @@
 var fs = require("fs");
-var cur_pid = parseInt(fs.readFileSync(".muon").toString());
-if (cur_pid != process.pid && !isNaN(cur_pid)){
-    console.log("Server already running: process id "+cur_pid);
-    process.exit();
-}
-fs.writeFileSync(".muon",process.pid);
 
 function exit(){
     fs.writeFileSync(".muon","");
