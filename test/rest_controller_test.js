8aea6fb68dfbf072bc644914f19bea135141012b
prikha <sergey@railscasts.ru>
2013-12-05 16:59:43 +0400

    action edit test added

--- test/rest_controller_test.js
+++ test/rest_controller_test.js
@@ -9,51 +9,57 @@ var serv = require('../module').server();
 
 
 describe('REST controller', function(){
+    var Mocha = this;
+
+//    start server
     before(function(done){
         serv.onready = done;
         this.rest = m.rest;
-    })
+    });
+//    drop database
+    before(function(done){
+        m.db.dropDatabase(function(err){
+            if(err){throw err};
+            done();
+        });
+    });
 
-    describe('action get', function(){
+    describe('action GET', function(){
+        before(function(done){
+            var User = m.models['user.user']
+            user = new User({nick:'prikha'});
+            user.save().then(function(user){
+                Mocha.test_user = user;
+                done();
+            })
+        })
         it("should be preset", function(){
             this.rest.actions.should.have.property('get');
         })
         it("should respond with valid object",function(done){
             var rest = this.rest;
+            var User = m.models['user.user'];
+            test_user = Mocha.test_user;
+            var req = {
+                context: {
+                    model: User,
+                    controller: rest,
+                    value: test_user.id,
+                    target: User
+                },
+                __compiledWhere__: {}
+            }
+            var res = {end: function(){}};
+            var ret = rest.actions.get.apply(req.context,[req,res,test_user.id]);
 
-            var User = m.models['user.user']
-
-            test_user = new User;
-            test_user.attributes = {nick:'prikha'}
-            test_user.save().then(function(){
-                m.log(test_user);
-
-                var req = {
-                    context: {
-                        model: User,
-                        controller: rest,
-                        value: test_user.id,
-                        target: User
-                    },
-                    __compiledWhere__: {}
-                }
-
-                var res = {end: function(){}};
-
-                var ret = rest.actions.get.apply(req.context,[req,res,test_user.id]);
-
-                Q.when(ret).done(function(user){
-                    m.log(user);
-                    user.should.eql(test_user);
-                    done();
-                });
-
+            Q.when(ret).done(function(user){
+                user.should.eql(test_user);
+                done();
             });
-
         })
-    })
+    });
 
-    describe('action index', function(){
+    describe('action INDEX', function(){
         before(function(done){
             this.User = m.models['user.user']
             this.User.db.find().done(function(collection){
@@ -61,19 +67,17 @@ describe('REST controller', function(){
                 done();
             });
         })
-
         it("should be present",function(){
             this.rest.actions.should.have.property('index');
         })
         it("should respond with collection", function(done){
             var User = this.User;
-            var UserCollection = User.__collection;
 
             var req = {
                 context:{
                     model: User,
                     target: User,
-                    context: this.rest
+                    controller: rest
                 },
                 __compiledWhere__: {}
             }
@@ -81,15 +85,97 @@ describe('REST controller', function(){
             var res = {end: function(){}}
 
             var ret = this.rest.actions.index.apply(req.context,[req,res,null]);
+
             Q.when(ret).done(function(collection){
                 var data = collection.slice();
-                m.log(data);
                 data.should.eql(this.test_collection);
                 done();
             });
 
         })
-    })
+    });
+
+    describe('action CREATE', function() {
+        before(function(done){
+            this.User.__collection.count(function(err,count){
+                if(err){throw err;}
+                this.before_user_count = count;
+                done();
+            });
+        });
+        it('should be present', function () {
+            this.rest.actions.should.have.property('create');
+        })
+        it('should create record', function(done){
+            var User = this.User;
+            var test_user = {nick: 'create_test_user',email: 'test@email.com'};
+            var req = {
+                context: {
+                    model: User,
+                    target: User,
+                    controller: rest,
+                    value: null,
+                    data: test_user
+                },
+                __compiledWhere__: {}
+            };
+
+            var res = {end: function(){}}
+
+            var ret = this.rest.actions.create.apply(req.context,[req,res,test_user]);
+            Q.when(ret).then(function(user){
+                    User.__collection.count(function(err,count){
+                        this.after_user_count = count;
+                        this.before_user_count.should.equal(this.after_user_count-1);
+                        (user instanceof User).should.equal(true);
+                       done();
+                    });
+            })
+
+        })
+    });
+
+    describe('action EDIT', function() {
+        //    save one user as this.existing_user
+        before(function(done){
+            var User = this.User;
+            var new_user = new User({nick: 'before_edit'});
+            new_user.save().done(function(user){
+                Mocha.existing_user = user;
+                done();
+            });
+        });
+
+        it('should be present', function () {
+            this.rest.actions.should.have.property('edit');
+        })
+
+        it('should change record', function(done){
+            var User = this.User;
+            var changed_user = {nick: 'after_edit'};
+            var existing_user = Mocha.existing_user;
+            var req = {
+                context: {
+                    model: User,
+                    target: User,
+                    controller: rest,
+                    value: existing_user.id,
+                    data: changed_user
+                },
+                __compiledWhere__: {}
+            };
+
+            var res = {end: function(){}}
+            var ret = this.rest.actions.edit.apply(req.context,[req,res,existing_user.id]);
+
+            Q.when(ret).then(function(user){
+                m.log('====');
+                m.log(user);
+                done();
+            })
+
+        })
+    });
 })
 
 
