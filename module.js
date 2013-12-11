e0602e5d3b0d0253eb0cd105d8ae1337dec7600f
Sukharev Kirill <SukharevKirill@gmail.com>
2013-12-11 18:17:05 +0400

    muon server onready -> callback to handler register; muon.server called with desired options as JSON; server.listen fixed and .close method added

--- module.js
+++ module.js
@@ -9,7 +9,8 @@ process.on('SIGINT',exit);
 process.on('exit',exit);
 
 module.exports = {
-    "server": function() {
+    "server": function(cfg) {
+        global.__cfg__ = cfg;
         return require("./lib/app.js")
     },
     "plugin" : function() {
