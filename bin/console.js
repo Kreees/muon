2a5ac3a1b0d9daab9265d1f9264a4aac9c5adf9a
Sergey Prikhodko <prikha@gmail.com>
2013-12-16 17:09:42 +0400

    space after prompt

--- bin/console.js
+++ bin/console.js
@@ -2,7 +2,7 @@
 
 var muon = require('./../module');
 muon.ready(function(){
-    var repl = require("repl").start('>');
+    var repl = require("repl").start('> ');
     var context = repl.context;
     context.m = muon;
     require('repl.history')(repl, process.env.HOME + '/.node_history');
