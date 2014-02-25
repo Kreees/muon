1147cd4bc3cb8e2d5ead67085516fd75e799e2a8
Sergey Prikhodko <prikha@gmail.com>
2014-02-25 17:24:51 +0400

    REST tests for muon api

--- test/acceptance/user_api.js
+++ test/acceptance/user_api.js
@@ -1,21 +1,27 @@
-var superagent = require('superagent');
 var request = require('supertest');
 
 var test_user;
-var agent = superagent.agent();
 
-before(function(done){
-   this.server = m.server().listen(8000,"localhost",done);
-})
 
-before(function(done){
+var clearUsers = function(done){
     var User = m.models['user'];
     User.find().remove(done);
+}
+
+before(function(done) {
+   this.server = m.server().listen(8000,"localhost",done);
 });
+
+before(function(done) {
+    var User = m.models['user'];
+    User = m.models['user'];
+    done();
+});
+
+before(function(done) { clearUsers(done); });
 
 before(function(done){
     var User = m.models['user'];
-    console.log(m.utils._.keys(m.models));
     var user = new User({nick:'prikha'});
     user.save(function(){
         test_user = user;
@@ -23,41 +29,91 @@ before(function(done){
     });
 });
 
-
-//after(function(done){
-//    User.find().done(function(query_set){
-//        query_set.del().done(function(){done()})});
-//});
-
-after(function(done){
-    this.server.close(done);
-});
-
 describe('REST', function(){
     describe('#index', function(){
-        it('should return json', function(done){
+        it('should return collection', function(done){
             request('http://localhost:8000')
                 .get('/apis/user?muon')
                 .expect('Content-Type', /json/)
-                .expect ([test_user])
-                .expect(200, done);
-        });
-
-//        it('should be success', function(done){
-//            agent.get('http://0.0.0.0:8000/apis/user?muon')
-//                .end(function(err,res){
-//                    should.not.exist(err);
-//
-//                    res.status.should.eql(200);
-//                    res.headers['content-type'].should.match(/application\/json/);
-//                    console.log("=========");
-//                    console.log(m.utils._.keys(res.body));
-//                    console.log("=========");
-//
-//                    done();
-//                })
-//        });
+                .expect(200)
+                .end(function(err,res){
+                    if(err){ return done(err) }
+                    res.body.should.be.instanceOf(Array);
+                    res.body.length.should.eq(1);
+                    res.body[0]["_id"].should.eq(test_user._id);
+                    done();
+                })
+            });
     });
-
+    describe('#show', function(){
+        it('should return resource', function(done){
+                request('http://localhost:8000')
+                    .get('/apis/user/'+test_user._id+'?muon')
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end(function(err,res){
+                        if(err){ return done(err) }
+                        resource = res.body
+                        resource.should.be.instanceOf(Object);
+                        resource._id.should.be.equal(test_user._id);
+                        m.utils._.keys(resource).should.include('nick', 'sex')
+                        done();
+                    })
+            });
+    });
+    describe('#create', function(){
+        it('should create resource', function(done){
+                request('http://localhost:8000')
+                    .post('/apis/user/?muon')
+                    .send({ nick: 'Kirill'})
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end(function(err,res){
+                        if(err){ return done(err) }
+                        resource = res.body;
+                        resource._id.should.not.eq(null);
+                        done();
+                    })
+            }
+        );
+    });
+    describe('#update', function(){
+        it('should change existing resource', function(done){
+                request('http://localhost:8000')
+                    .put('/apis/user/'+test_user._id+'?muon')
+                    .send({ nick: 'Sergey'})
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end(function(err,res){
+                        if(err){ return done(err) }
+                        resource = res.body;
+                        resource.nick.should.equal('Sergey');
+                        resource.modified_at.should.not.equal(test_user.modified_at)
+                        resource.created_at.should.not.equal(test_user.created_at)
+                        done();
+                    })
+            }
+        );
+    });
+    describe('#delete', function(){
+        it('should create resource', function(done){
+                request('http://localhost:8000')
+                    .del('/apis/user/'+test_user._id+'?muon')
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end(function(err,res){
+                        if(err){ return done(err) }
+                        resource = res.body;
+                        var User = m.models['user']
+                        User.get(resource._id,function(user){
+                            user.code.should.equal(2)
+                            done();
+                        })
+                    })
+            });
+    });
 });
 
+after(function(done) { clearUsers(done); });
+
+after(function(done) { this.server.close(done); });
