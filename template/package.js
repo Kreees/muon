aeed9f267c3a06f934963f55471700243c6e1b8d
LavN <ns.lavrenko@mail.ru>
2014-09-16 17:27:38 +0400

    add

---
+++ template/package.js
@@ -0,0 +1,22 @@
+{
+    "name": "#{project}",
+    "version": "0.0.1",
+    "author":"#{user}",
+    "main": "./module.js",
+    "dependencies": {
+        "muon": "*"
+    },
+    "devDependencies": {
+        "async": "*",
+        "q":"*",
+        "underscore":"*",
+        "mocha": "*",
+        "supertest": "*",
+        "superagent": "*",
+        "chai": "*",
+        "chai-spies": "*",
+        "node-mocks-http":"*",
+        "mocha-as-promised": "~1.4.1",
+        "repl.history":"*"
+    }
+}
